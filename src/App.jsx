46aa9ca670bce87948612a6a7165091fe5471d2d
Eziio <thphong16902@gmail.com>
2025-05-12 16:04:08 +0700

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold underline bg-red-500 text-white-50">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    tailwindcss()
+  ],
+  resolve: { // for absolute imports
+    alias: [{ find: '~', replacement: '/src' }]
+  }
+  // base: './'
+})
