b9df6bc02bcd82ad7584b4c286952747f038bdd2
Eziio <thphong16902@gmail.com>
2025-07-29 16:30:08 +0700

    Add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 import MainLayout from './layouts/MainLayout'
 import MovieLayout from './layouts/MovieLayout'
@@ -10,58 +10,54 @@ import Category from './pages/Category'
 import ScrollToTop from "~/components/ScrollToTop.jsx";
 
 function App() {
+    return (
+        <>
+            <ScrollToTop/>
+            <Routes>
+                {/* Redirect Routes */}
+                <Route path='/' element={
+                    <Navigate to='/home' replace={true}/>
+                }/>
 
-  return (
-      <>
-          <ScrollToTop />
-          <Routes>
-          {/* Redirect Routes */}
-          <Route path='/' element={
-              <Navigate to='/home' replace={true}/>
-          }/>
+                {/* Home Page */}
+                <Route path='/home' element={
+                    <MainLayout>
+                        <Home/>
+                    </MainLayout>
+                }/>
 
-          {/* Home Page */}
-          <Route path='/home' element={
-              <MainLayout>
-                  <Home/>
-              </MainLayout>
-          }/>
+                {/* Category Page */}
+                <Route path='/category/:categoryId' element={
+                    <MainLayout>
+                        <Category/>
+                    </MainLayout>
+                }/>
 
-          {/* Category Page */}
-          <Route path='/category/:categoryId' element={
-              <MainLayout>
-                  <Category/>
-              </MainLayout>
-          }/>
+                {/* Movies */}
+                <Route path='/movies' element={
+                    <MainLayout>
+                        {/*<Home/>*/}
+                    </MainLayout>
+                }/>
 
-          {/* Movies */}
-          <Route path='/movies' element={
-              <MainLayout>
-                  {/*<Home/>*/}
-              </MainLayout>
-          }/>
+                {/* Movie Details */}
+                <Route path='/movies/:movieId' element={
+                    <MovieLayout>
+                        <Movie/>
+                    </MovieLayout>
+                }/>
 
-          {/* Movie Details */}
-          <Route path='/movies/:movieId' element={
-              <MovieLayout>
-                  <Movie/>
-              </MovieLayout>
-          }/>
-
-          {/* Watch Page */}
-          <Route path='/movies/watch/:movieId' element={
-              <WatchLayout>
-                  <Watch/>
-              </WatchLayout>
-          }/>
-
-          {/* Login - Reister */}
-
-
-          {/* 404 page */}
-
-      </Routes></>
-  )
+                {/* Watch Page */}
+                <Route path='/movies/watch/:movieId' element={
+                    <WatchLayout>
+                        <Watch/>
+                    </WatchLayout>
+                }/>
+                {/* Login - Reister */}
+                {/* 404 page */}
+            </Routes>
+        </>
+    )
 }
 
 export default App
